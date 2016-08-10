72367dd6c52ce2be074143bcaa24248af6cd954e
hy <cengling17@hotmail.com>
2016-08-10 12:01:02 +0800

    hy <cengling17@hotmail.com>

--- BCP.WebAPI/Controllers/UserController.cs
+++ BCP.WebAPI/Controllers/UserController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Newtonsoft.Json;
 using System.Text;
+using BCP.WebAPI.Helpers;
 
 namespace BCP.WebAPI.Controllers
 {
@@ -19,19 +20,23 @@ namespace BCP.WebAPI.Controllers
 
         public HttpResponseMessage RegisterUser(UserDTO user)
         {
-
-            if (UserService.RegisterUser(user))
+            try
             {
-                String str=JsonConvert.SerializeObject(UserService.GetUser(user.UserName));
-                //JavaScriptSerializer serializer = new JavaScriptSerializer();
-                //string str = serializer.Serialize(user);
-                HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(str, Encoding.GetEncoding("UTF-8"), "application/json") };
-                return result; 
+                if (UserService.RegisterUser(user))
+                {
+                    //String str=JsonConvert.SerializeObject(UserService.GetUser(user.UserName));
+                    //JavaScriptSerializer serializer = new JavaScriptSerializer();
+                    //string str = serializer.Serialize(user);
+                    //HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(str, Encoding.GetEncoding("UTF-8"), "application/json") };
+                    //return result; 
+                    return JsonHelper.GetResponseMessage(true, "登录成功", typeof(UserDTO), false, UserService.GetUser(user.UserName));
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return new HttpResponseMessage { Content = new StringContent("false", Encoding.GetEncoding("UTF-8"), "application/json") };
+                return JsonHelper.GetResponseMessage(false, "登录失败"+ex.Message, null, false, null);
             }
+            return JsonHelper.GetResponseMessage(false, "登录失败", null, false, null);
         }
     }
 }
